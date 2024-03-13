cd73561565f6f04da2cff8a5e204f3df7df54100
John Pascual <ashleypascual@hotmail.es>
2024-03-13 23:45:14 +0000

    add button shadow and counter functionality

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const button = document.querySelector("button");
-const advice = document.querySelector("p")
+const adviceText = document.querySelector("p");
+const adviceCounter = document.querySelector("h1");
 
-button.addEventListener("click", fetchAdvice)
+button.addEventListener("click", fetchAdvice);
+
+let counter = 1;
 
 async function fetchAdvice() {
-    const response = await fetch("https://api.adviceslip.com/advice")
-    const data = await response.json()
-    advice.innerHTML = `${data.slip.advice}`
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    adviceText.innerHTML = `"${data.slip.advice}"`;
+    adviceCounter.innerHTML = `ADVICE #${counter + 1}`;
+    counter++;
 }
